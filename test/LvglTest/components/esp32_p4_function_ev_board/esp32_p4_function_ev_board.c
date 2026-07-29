0dba3322e03755daf4a53679257d527ddcae4aa2
Kangwoo Cheon <kwcheon@astams.com>
2026-07-29 17:16:07 +0900

    run LVGL widget demo

--- test/LvglTest/components/esp32_p4_function_ev_board/esp32_p4_function_ev_board.c
+++ test/LvglTest/components/esp32_p4_function_ev_board/esp32_p4_function_ev_board.c
@@ -24,7 +24,7 @@
 #include "esp_lcd_ek79007.h"
 #elif CONFIG_BSP_LCD_TYPE_HDMI
 #include "esp_lcd_lt8912b.h"
-#elif BSP_LCD_TYPE_480_800
+#elif CONFIG_BSP_LCD_TYPE_480_800
 #include "esp_lcd_st7701.h"
 #else
 #include "esp_lcd_ili9881c.h"
@@ -40,6 +40,236 @@
 #include "esp_video_init.h"
 
 static const char *TAG = "ESP32_P4_EV";
+
+
+#if CONFIG_BSP_LCD_TYPE_480_800
+
+#include "esp_lcd_mipi_dsi.h"
+#include "esp_ldo_regulator.h"
+
+#define LCD_H_RES              480
+#define LCD_V_RES              800
+#define LCD_DPI_CLK_MHZ        34 // 25
+
+#define LCD_HSYNC              12  // 10
+#define LCD_HBP                42 // 40
+#define LCD_HFP                42 // 40
+#define LCD_VSYNC              2  // 4
+#define LCD_VBP                8  // 13
+#define LCD_VFP                166 // 2
+
+#define MIPI_DSI_LANE_NUM      2
+#define MIPI_DSI_LANE_RATE     1000
+
+#define MIPI_PHY_LDO_CHAN      3
+#define MIPI_PHY_LDO_MV       2500
+
+#define LCD_RST_PIN            5
+#define LCD_BK_LIGHT_PIN       23
+
+
+
+static const st7701_lcd_init_cmd_t lcd_init_cmds[] = {
+    {0xFF, (uint8_t []){0x77,0x01,0x00,0x00,0x13}, 5, 0},
+    {0xEF, (uint8_t []){0x08}, 1, 0},
+    {0xFF, (uint8_t []){0x77,0x01,0x00,0x00,0x10}, 5, 0},
+    {0xC0, (uint8_t []){0x63, 0x00}, 2, 0},
+    {0xC1, (uint8_t []){0x0D, 0x02}, 2, 0},
+    {0xC2, (uint8_t []){0x10, 0x08}, 2, 0},
+    {0xCC, (uint8_t []){0x10}, 1, 0},
+    {0xB0, (uint8_t []){0x80,0x09,0x53,0x0C,0xD0,0x07,0x0C,0x09,0x09,0x28,0x06,0xD4,0x13,0x69,0x2B,0x71}, 16, 0},
+    {0xB1, (uint8_t []){0x80,0x94,0x5A,0x10,0xD3,0x06,0x0A,0x08,0x08,0x25,0x03,0xD3,0x12,0x66,0x6A,0x0D}, 16, 0},
+
+    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x11}, 5, 0},
+    {0xB0, (uint8_t []){0x5D}, 1, 0},
+    {0xB1, (uint8_t []){0x58}, 1, 0},
+    {0xB2, (uint8_t []){0x87}, 1, 0},
+    {0xB3, (uint8_t []){0x80}, 1, 0},
+    {0xB5, (uint8_t []){0x4E}, 1, 0},
+    {0xB7, (uint8_t []){0x85}, 1, 0},
+    {0xB8, (uint8_t []){0x21}, 1, 0},
+    {0xB9, (uint8_t []){0x10, 0x1F}, 2, 0},
+    {0xBB, (uint8_t []){0x03}, 1, 0},
+    {0xBC, (uint8_t []){0x00}, 1, 0},
+    {0xC1, (uint8_t []){0x78}, 1, 0},
+    {0xC2, (uint8_t []){0x78}, 1, 0},
+    {0xD0, (uint8_t []){0x88}, 1, 100},
+    {0xE0, (uint8_t []){0x00, 0x3A, 0x02}, 3, 0},
+    {0xE1, (uint8_t []){0x04,0xA0,0x00,0xA0,0x05,0xA0,0x00,0xA0,0x00,0x40,0x40}, 11, 0},
+    {0xE2, (uint8_t []){0x30,0x00,0x40,0x40,0x32,0xA0,0x00,0xA0,0x00,0xA0,0x00,0xA0,0x00}, 13, 0},
+    {0xE3, (uint8_t []){0x00,0x00,0x33,0x33}, 4, 0},
+    {0xE4, (uint8_t []){0x44, 0x44}, 2, 0},
+    {0xE5, (uint8_t []){0x09,0x2E,0xA0,0xA0,0x0B,0x30,0xA0,0xA0,0x05,0x2A,0xA0,0xA0,0x07,0x2C,0xA0,0xA0}, 16, 0},
+    {0xE6, (uint8_t []){0x00,0x00,0x33,0x33}, 4, 0},
+    {0xE7, (uint8_t []){0x44, 0x44}, 2, 0},
+    {0xE8, (uint8_t []){0x08,0x2D,0xA0,0xA0,0x0A,0x2F,0xA0,0xA0,0x04,0x29,0xA0,0xA0,0x06,0x2B,0xA0,0xA0}, 16, 0},
+    {0xEB, (uint8_t []){0x00,0x00,0x4E,0x4E,0x00,0x00,0x00}, 7, 0},
+    {0xEC, (uint8_t []){0x08, 0x01}, 2, 0},
+    {0xED, (uint8_t []){0xB0,0x2B,0x98,0xA4,0x56,0x7F,0xFF,0xFF,0xFF,0xFF,0xF7,0x65,0x4A,0x89,0xB2,0x0B}, 16, 0},
+    {0xEF, (uint8_t []){0x08,0x08,0x08,0x45,0x3F,0x54}, 6, 0},
+    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x00}, 5, 0},
+
+    {0x11, (uint8_t []){0x00}, 0, 120},
+    {0x29, (uint8_t []){0x00}, 0, 20},
+};
+
+
+
+
+void disp_init(esp_lcd_panel_handle_t* panel_handle)
+{
+    ESP_LOGI(TAG, "[LCD] Init backlight pin");
+    gpio_config_t bk_io_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = (uint64_t)1ULL << LCD_BK_LIGHT_PIN,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_io_config));
+    gpio_set_level((gpio_num_t)LCD_BK_LIGHT_PIN, 0);
+
+    ESP_LOGI(TAG, "[LCD] Initializing MIPI DSI PHY power");
+    esp_ldo_channel_handle_t ldo_mipi = NULL;
+    esp_ldo_channel_config_t ldo_config = {
+        .chan_id = MIPI_PHY_LDO_CHAN,
+        .voltage_mv = MIPI_PHY_LDO_MV,
+    };
+    ESP_ERROR_CHECK(esp_ldo_acquire_channel(&ldo_config, &ldo_mipi));
+
+    ESP_LOGI(TAG, "[LCD] Create MIPI DSI bus");
+    esp_lcd_dsi_bus_handle_t dsi_bus = NULL;
+    esp_lcd_dsi_bus_config_t bus_config = {
+        .bus_id = 0,
+        .num_data_lanes = MIPI_DSI_LANE_NUM,
+        .lane_bit_rate_mbps = MIPI_DSI_LANE_RATE,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_dsi_bus(&bus_config, &dsi_bus));
+
+    ESP_LOGI(TAG, "[LCD] Create DBI IO");
+    esp_lcd_panel_io_handle_t dbi_io = NULL;
+    esp_lcd_dbi_io_config_t dbi_config = {
+        .virtual_channel = 0,
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_dbi(dsi_bus, &dbi_config, &dbi_io));
+
+    ESP_LOGI(TAG, "[LCD] Create ST7701 DPI panel");
+    esp_lcd_dpi_panel_config_t dpi_config = {
+        .virtual_channel = 0,
+        .dpi_clk_src = MIPI_DSI_DPI_CLK_SRC_DEFAULT,
+        .dpi_clock_freq_mhz = LCD_DPI_CLK_MHZ,
+        .in_color_format = LCD_COLOR_FMT_RGB565,
+        .video_timing = {
+            .h_size = LCD_H_RES,
+            .v_size = LCD_V_RES,
+            .hsync_pulse_width = LCD_HSYNC,
+            .hsync_back_porch = LCD_HBP,
+            .hsync_front_porch = LCD_HFP,
+            .vsync_pulse_width = LCD_VSYNC,
+            .vsync_back_porch = LCD_VBP,
+            .vsync_front_porch = LCD_VFP,
+        },
+    };
+    st7701_vendor_config_t vendor_config = {
+        .init_cmds = lcd_init_cmds,
+        .init_cmds_size = sizeof(lcd_init_cmds) / sizeof(lcd_init_cmds[0]),
+        .flags.use_mipi_interface = 1,
+        .mipi_config = {
+            .dsi_bus = dsi_bus,
+            .dpi_config = &dpi_config,
+        },
+    };
+    esp_lcd_panel_dev_config_t dev_config = {
+        .reset_gpio_num = LCD_RST_PIN,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = 16,
+        .vendor_config = &vendor_config,
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7701(dbi_io, &dev_config, panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(*panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(*panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(*panel_handle, true));
+    gpio_set_level((gpio_num_t)LCD_BK_LIGHT_PIN, 1);
+
+    ESP_LOGI(TAG, "[LCD] Initialized");
+}
+
+
+#define TOUCH_I2C_SDA_PIN      7
+#define TOUCH_I2C_SCL_PIN      8
+#define TOUCH_RST_PIN          22
+#define TOUCH_INT_PIN          21
+#define TOUCH_I2C_SPEED_HZ     400000
+
+esp_lcd_touch_handle_t tp_handle = NULL;
+
+void touch_init()
+{
+    ESP_LOGI(TAG, "[TOUCH] Initialize GT911");
+    i2c_master_bus_config_t i2c_bus_config = {
+        .i2c_port = I2C_NUM_0,
+        .sda_io_num = TOUCH_I2C_SDA_PIN,
+        .scl_io_num = TOUCH_I2C_SCL_PIN,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    i2c_master_bus_handle_t i2c_bus = NULL;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &i2c_bus));
+
+    esp_lcd_panel_io_handle_t tp_io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_GT911_CONFIG();
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &tp_io_config, &tp_io_handle));
+
+    esp_lcd_touch_io_gt911_config_t tp_gt911_config = {
+        .dev_addr = tp_io_config.dev_addr,
+    };
+    esp_lcd_touch_config_t tp_cfg = {
+        .x_max = LCD_H_RES,
+        .y_max = LCD_V_RES,
+        .rst_gpio_num = TOUCH_RST_PIN,
+        .int_gpio_num = TOUCH_INT_PIN,
+        .levels = {
+            .reset = 0,
+            .interrupt = 0,
+        },
+        .flags = {
+            .swap_xy = 0,
+            .mirror_x = 0,
+            .mirror_y = 0,
+        },
+        .driver_data = &tp_gt911_config,
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_touch_new_i2c_gt911(tp_io_handle, &tp_cfg, &tp_handle));
+    ESP_LOGI(TAG, "[TOUCH] GT911 initialized");
+}
+
+
+bool touch_driver_read(int16_t* x, int16_t* y)
+{
+    esp_lcd_touch_point_data_t point_data;
+    uint8_t cnt = 0;
+
+    esp_lcd_touch_read_data(tp_handle);
+    if (esp_lcd_touch_get_data(tp_handle, &point_data, &cnt, 1) == ESP_OK && cnt > 0) 
+    {
+        *x = point_data.x;
+        *y = point_data.y;
+        return true;
+    }
+
+    return false;
+}
+
+
+#endif // CONFIG_BSP_LCD_TYPE_480_800
+
+
+
 
 #if (BSP_CONFIG_NO_GRAPHIC_LIB == 0)
 static lv_indev_t *disp_indev = NULL;
@@ -475,6 +705,7 @@ esp_codec_dev_handle_t bsp_audio_codec_microphone_init(void)
 
 esp_err_t bsp_display_brightness_init(void)
 {
+    #if 1
     // Setup LEDC peripheral for PWM backlight control
     const ledc_channel_config_t LCD_backlight_channel = {
         .gpio_num = BSP_LCD_BACKLIGHT,
@@ -490,16 +721,32 @@ esp_err_t bsp_display_brightness_init(void)
         .duty_resolution = LEDC_TIMER_10_BIT,
         .timer_num = 1,
         .freq_hz = 5000,
-        .clk_cfg = LEDC_AUTO_CLK
+        .clk_cfg = LEDC_USE_XTAL_CLK //LEDC_AUTO_CLK
     };
 
+    ESP_LOGI(TAG, "1-0");
     BSP_ERROR_CHECK_RETURN_ERR(ledc_timer_config(&LCD_backlight_timer));
+    ESP_LOGI(TAG, "1-1");
     BSP_ERROR_CHECK_RETURN_ERR(ledc_channel_config(&LCD_backlight_channel));
+    ESP_LOGI(TAG, "1-2");
+    #else
+    ESP_LOGI(TAG, "[LCD] Init backlight pin");
+    gpio_config_t bk_io_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << BSP_LCD_BACKLIGHT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_io_config));
+    gpio_set_level(BSP_LCD_BACKLIGHT, 0);
+    #endif
     return ESP_OK;
 }
 
 esp_err_t bsp_display_brightness_deinit(void)
 {
+    #if 1
     const ledc_timer_config_t LCD_backlight_timer = {
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .timer_num = 1,
@@ -507,11 +754,13 @@ esp_err_t bsp_display_brightness_deinit(void)
     };
     BSP_ERROR_CHECK_RETURN_ERR(ledc_timer_pause(LEDC_LOW_SPEED_MODE, 1));
     BSP_ERROR_CHECK_RETURN_ERR(ledc_timer_config(&LCD_backlight_timer));
+    #endif
     return ESP_OK;
 }
 
 esp_err_t bsp_display_brightness_set(int brightness_percent)
 {
+    #if 1
     if (brightness_percent > 100) {
         brightness_percent = 100;
     }
@@ -523,6 +772,12 @@ esp_err_t bsp_display_brightness_set(int brightness_percent)
     uint32_t duty_cycle = (1023 * brightness_percent) / 100; // LEDC resolution set to 10bits, thus: 100% = 1023
     BSP_ERROR_CHECK_RETURN_ERR(ledc_set_duty(LEDC_LOW_SPEED_MODE, LCD_LEDC_CH, duty_cycle));
     BSP_ERROR_CHECK_RETURN_ERR(ledc_update_duty(LEDC_LOW_SPEED_MODE, LCD_LEDC_CH));
+    #else
+    if (brightness_percent > 0)
+        gpio_set_level(BSP_LCD_BACKLIGHT, 1);
+    else
+        gpio_set_level(BSP_LCD_BACKLIGHT, 0);
+    #endif
     return ESP_OK;
 }
 
@@ -773,8 +1028,45 @@ esp_err_t bsp_display_new_with_handles(const bsp_display_config_t *config, bsp_l
     ESP_GOTO_ON_ERROR(esp_lcd_panel_reset(disp_panel), err, TAG, "LCD panel reset failed");
     ESP_GOTO_ON_ERROR(esp_lcd_panel_init(disp_panel), err, TAG, "LCD panel init failed");
 
-#elif BSP_LCD_TYPE_480_800
+#elif CONFIG_BSP_LCD_TYPE_480_800
+    // create ST7701 control panel
+    ESP_LOGI(TAG, "Install ST7701 LCD control panel");
 
+    esp_lcd_dpi_panel_config_t dpi_config = {
+        .virtual_channel = 0,
+        .dpi_clk_src = MIPI_DSI_DPI_CLK_SRC_DEFAULT,
+        .dpi_clock_freq_mhz = LCD_DPI_CLK_MHZ,
+        .in_color_format = LCD_COLOR_FMT_RGB565,
+        .video_timing = {
+            .h_size = LCD_H_RES,
+            .v_size = LCD_V_RES,
+            .hsync_pulse_width = LCD_HSYNC,
+            .hsync_back_porch = LCD_HBP,
+            .hsync_front_porch = LCD_HFP,
+            .vsync_pulse_width = LCD_VSYNC,
+            .vsync_back_porch = LCD_VBP,
+            .vsync_front_porch = LCD_VFP,
+        },
+    };
+    st7701_vendor_config_t vendor_config = {
+        .init_cmds = lcd_init_cmds,
+        .init_cmds_size = sizeof(lcd_init_cmds) / sizeof(lcd_init_cmds[0]),
+        .flags.use_mipi_interface = 1,
+        .mipi_config = {
+            .dsi_bus = mipi_dsi_bus,
+            .dpi_config = &dpi_config,
+        },
+    };
+    esp_lcd_panel_dev_config_t dev_config = {
+        .reset_gpio_num = BSP_LCD_RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = 16,
+        .vendor_config = &vendor_config,
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7701(io, &dev_config, &disp_panel));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(disp_panel));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(disp_panel));
 #endif //CONFIG_BSP_LCD_TYPE_
 
     /* Return all handles */
@@ -1029,14 +1321,17 @@ lv_display_t *bsp_display_start_with_config(const bsp_display_cfg_t *cfg)
     lv_display_t *disp;
 
     assert(cfg != NULL);
+    ESP_LOGI(TAG, "0");
+    ESP_ERROR_CHECK(bsp_display_brightness_init());
+    ESP_LOGI(TAG, "0-a");
     BSP_ERROR_CHECK_RETURN_NULL(lvgl_port_init(&cfg->lvgl_port_cfg));
-
-    BSP_ERROR_CHECK_RETURN_NULL(bsp_display_brightness_init());
-
+    ESP_LOGI(TAG, "1");
     BSP_NULL_CHECK(disp = bsp_display_lcd_init(cfg), NULL);
+    ESP_LOGI(TAG, "3");
 #if !CONFIG_BSP_LCD_TYPE_HDMI
     BSP_NULL_CHECK(disp_indev = bsp_display_indev_init(disp), NULL);
 #endif
+    ESP_LOGI(TAG, "4");
     return disp;
 }
 
--- test/LvglTest/main/main.cpp
+++ test/LvglTest/main/main.cpp
@@ -12,21 +12,298 @@
 #include "esp_memory_utils.h"
 #include "bsp/esp-bsp.h"
 
+#include "lvgl.h"
 #include "demos/lv_demos.h"
 
+static const char *TAG = "MAIN";
+
 
 //
 //
 //
+
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_st7701.h"
+#include "esp_lcd_mipi_dsi.h"
+#include "esp_ldo_regulator.h"
+
+#define LCD_H_RES              480
+#define LCD_V_RES              800
+#define LCD_DPI_CLK_MHZ        34 // 25
+
+#define LCD_HSYNC              12  // 10
+#define LCD_HBP                42 // 40
+#define LCD_HFP                42 // 40
+#define LCD_VSYNC              2  // 4
+#define LCD_VBP                8  // 13
+#define LCD_VFP                166 // 2
+
+#define MIPI_DSI_LANE_NUM      2
+#define MIPI_DSI_LANE_RATE     1000
+
+#define MIPI_PHY_LDO_CHAN      3
+#define MIPI_PHY_LDO_MV       2500
+
+#define LCD_RST_PIN            5
+#define LCD_BK_LIGHT_PIN       23
+
+extern "C" void disp_init(esp_lcd_panel_handle_t* panel_handle);
+extern "C" void touch_init();
+extern "C" bool touch_driver_read(int16_t* x, int16_t* y);
+
+
+static bool notify_color_trans_done(esp_lcd_panel_handle_t panel, esp_lcd_dpi_panel_event_data_t *edata, void *user_ctx) 
+{
+    lv_display_t *disp = (lv_display_t *)user_ctx;
+    // LVGL v9: 플러시 완료 통보
+    //ESP_LOGI(TAG, "[] notify_color_trans_done()");
+    lv_display_flush_ready(disp);
+    return false;
+}
+
+static  void my_disp_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) 
+{
+    //ESP_LOGI(TAG, "[] my_disp_flush_cb(START)");
+    esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t)lv_display_get_user_data(disp);
+    
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+
+    // esp_lcd를 통해 화면에 픽셀 데이터 전송
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, px_map);
+    //lv_display_flush_ready(disp); 
+    //ESP_LOGI(TAG, "[] my_disp_flush_cb(DONE)");
+}
+
+
+
+static void my_touchpad_read_cb(lv_indev_t * indev, lv_indev_data_t * data)
+{
+    int16_t touch_x = 0;
+    int16_t touch_y = 0;
+    bool is_pressed = false;
+
+    // TODO: 개발자님이 만드신 기존 터치 드라이버 함수를 여기에 호출하세요.
+    is_pressed = touch_driver_read(&touch_x, &touch_y);
+
+    if(is_pressed) {
+        data->state = LV_INDEV_STATE_PRESSED;
+        data->point.x = touch_x;
+        data->point.y = touch_y;
+    } else {
+        data->state = LV_INDEV_STATE_RELEASED;
+    }
+}
+
+void lvgl_touch_init(void)
+{
+    // 입력 장치 객체 생성
+    lv_indev_t * indev = lv_indev_create();
+    
+    // 장치 타입을 포인터(터치스크린, 마우스 등)로 설정
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    
+    // 위에서 만든 읽기 콜백 함수 등록
+    lv_indev_set_read_cb(indev, my_touchpad_read_cb);
+}
+
+void lvgl_port_init(esp_lcd_panel_handle_t panel_handle) 
+{
+    // 1. LVGL 핵심 코어 초기화
+    ESP_LOGI(TAG, "lv_init()");
+    lv_init();
+
+#if 1
+    // 2. LVGL v9 디스플레이 객체 생성
+    ESP_LOGI(TAG, "lv_display_create()");
+    lv_display_t *disp = lv_display_create(LCD_H_RES, LCD_V_RES);
+    
+    // esp_lcd 패널 핸들을 유저 데이터로 저장 (콜백에서 사용)
+    ESP_LOGI(TAG, "lv_display_set_user_data()");
+    lv_display_set_user_data(disp, panel_handle);
+
+    // 3. 내부/외장 RAM 버퍼 할당 (v9은 1바이트 포인터 배열로 전달 가능)
+    ESP_LOGI(TAG, "heap_caps_malloc(1)");
+    void *buf1 = heap_caps_malloc(LCD_H_RES * LCD_V_RES * sizeof(uint16_t), MALLOC_CAP_SPIRAM);
+    //ESP_LOGI(TAG, "heap_caps_malloc(2)");
+    //void *buf2 = heap_caps_malloc(LCD_H_RES * LCD_V_RES * sizeof(uint16_t), MALLOC_CAP_SPIRAM); // 더블 버퍼 사용 시 (선택)
+    assert(buf1 != NULL);
+
+    // 디스플레이에 버퍼 등록
+    ESP_LOGI(TAG, "lv_display_set_buffers()");
+    lv_display_set_buffers(disp, buf1, 0/*buf2*/, LCD_H_RES * LCD_V_RES * sizeof(uint16_t), LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+    // 4. 플러시 콜백 등록
+    ESP_LOGI(TAG, "lv_display_set_flush_cb()");
+    lv_display_set_flush_cb(disp, my_disp_flush_cb);
+
+    // 5. esp_lcd 전송 완료 이벤트에 콜백 등록 (플러시 완료 연동)
+    //const esp_lcd_panel_dev_config_t *dev_cfg; // disp_init에서 설정한 config
+    // 주의: 실제 코드에서는 disp_init 내부에서 이 콜백을 등록하거나, panel_handle 생성이 끝난 직후 등록해야 합니다.
+    //TaskHandle_t trans_done_task = xTaskGetCurrentTaskHandle();
+    esp_lcd_dpi_panel_event_callbacks_t cbs = {
+        .on_color_trans_done = notify_color_trans_done,
+    };
+    ESP_LOGI(TAG, "esp_lcd_dpi_panel_register_event_callbacks()");
+    ESP_ERROR_CHECK(esp_lcd_dpi_panel_register_event_callbacks(panel_handle, &cbs, disp));
+#endif
+
+#if 1
+    //
+    lvgl_touch_init();
+#endif
+
+    ESP_LOGI(TAG, "lvgl_port_init() DONE");    
+}
+
+static void lvgl_tick_cb(void *arg) 
+{
+    lv_tick_inc(1);
+}
+
+void lvgl_task(void *arg) 
+{
+    // 1ms 주기 esp_timer 생성 및 시작
+    const esp_timer_create_args_t tick_timer_args = {
+        .callback = &lvgl_tick_cb,
+        .name = "lvgl_tick"
+    };
+    esp_timer_handle_t tick_timer;
+    esp_timer_create(&tick_timer_args, &tick_timer);
+    esp_timer_start_periodic(tick_timer, 1000); // 1000us = 1ms
+
+    // 2. LVGL 핸들러 루프 실행
+    ESP_LOGI(TAG, "Start LVGL main task");
+    while (1) 
+    {
+        // lv_timer_handler()는 다음 실행까지 필요한 대기 시간을 ms 단위로 반환합니다.
+        uint32_t task_delay_ms = lv_timer_handler();
+        /*
+        if (task_delay_ms < 1) {
+            task_delay_ms = 1;
+        } else if (task_delay_ms > 30) {
+            task_delay_ms = 30; // 지나치게 긴 대기 방지
+        }
+        */
+        //ESP_LOGI(TAG, "[*] delay_ms : %u", task_delay_ms);
+        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
+    }
+}
+
+
+static lv_obj_t * my_label;
+static int counter = 0;
+
+/* 1초마다 실행될 콜백 함수 */
+static void timer_callback(lv_timer_t * timer)
+{
+    counter++;
+    
+    /* 카운터 값에 따라 글자 변경 (printf 포맷 사용) */
+    lv_label_set_text_fmt(my_label, "Count: %d", counter);
+}
+
 
 extern "C" void app_main(void)
 {
+#if 1
     //
-    initArduino();
+    esp_lcd_panel_handle_t panel_handle = NULL;   
+    disp_init(&panel_handle); 
+    assert(panel_handle != NULL);
 
     //
+    touch_init();
+
+    //
+    lvgl_port_init(panel_handle);
+
+    #if 1
+
+    #if 1
+    lv_demo_widgets(); 
+    #else
+    {
+        lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x200000), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, LV_PART_MAIN);
+
+        /* 2. 텍스트 라벨 생성 */
+        lv_obj_t * label = lv_label_create(lv_screen_active());
+        my_label = label;
+        
+        /* 3. "Hello" 글자 설정 */
+        lv_label_set_text(label, "Hello");
+
+        /* 4. 왼쪽 상단 정렬 및 여백(Offset) 설정 */
+        lv_obj_align(label, LV_ALIGN_TOP_LEFT, 10, 10);
+
+        /* 5. 글자 색상 변경 (선택 사항: 빨간 배경에서 잘 보이도록 흰색 설정) */
+        lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+        //
+        lv_obj_set_style_text_font(label, &lv_font_montserrat_26, LV_PART_MAIN);
+
+
+        //
+        lv_timer_create(timer_callback, 1000, NULL);
+    }
+    #endif
+
+    #if 1
+    xTaskCreatePinnedToCore(
+        lvgl_task,          // 태스크 함수
+        "LVGL Task",        // 태스크 이름
+        1024 * 16,           // 스택 크기 (LVGL v9은 최소 4~5KB 이상 권장)
+        NULL,               // 전달 인자
+        5,                  // 태스크 우선순위 (비교적 높은 순위 부여)
+        NULL,               // 태스크 핸들
+        1                   // 실행할 CPU 코어 번호 (0 또는 1)
+    );
+    #else
+    lvgl_task(nullptr);
+    #endif
+    #endif
+
+    //
+    while (1) 
+    {
+        //ESP_LOGI(TAG, "1초마다 출력되는 로그입니다.");
+        
+        // 1000ms(1초) 동안 태스크를 대기 상태로 전환
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+#endif
+
+#if 0
+    //
+    //
+    initArduino();
+    //
+
+    Serial.begin(115200);
+    while (!Serial);
+
+    while (1)
+    {
+        Serial.println("Hello");
+        delay(1000);
+    }
+#endif
+
+#if 0
+    //
     bsp_display_cfg_t cfg = {
-        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+        .lvgl_port_cfg = {
+            .task_priority = 1,
+            .task_stack = 7168,
+            .task_affinity = -1,
+            .task_max_sleep_ms = 500,
+            .task_stack_caps = MALLOC_CAP_INTERNAL | MALLOC_CAP_DEFAULT,
+            .timer_period_ms = 5,
+        },
         .buffer_size = 480 * 800,
         .double_buffer = BSP_LCD_DRAW_BUFF_DOUBLE,
         .flags = {
@@ -35,8 +312,9 @@ extern "C" void app_main(void)
             .sw_rotate = true,
         }
     };
-    bsp_display_start_with_config(&cfg);
-    bsp_display_backlight_on();
+
+    //bsp_display_start();
+    lv_display_t* disp = bsp_display_start_with_config(&cfg);
 
     bsp_display_lock(0);
     {
@@ -44,5 +322,15 @@ extern "C" void app_main(void)
     }
     bsp_display_unlock();
 
-    bsp_display_brightness_set(100);
+    bsp_display_brightness_set(60);
+    bsp_display_backlight_on();
+
+    /*
+    while(1)
+    {
+        uint32_t time_till_next = lv_timer_handler();
+        lv_delay_ms(time_till_next);
+    }
+    */
+#endif   
 }
